a41a17c1527f39d820f2f99af1df2946fd13de61
Nandinee Sharma <141727975+Nandinee-Sharma@users.noreply.github.com>
2024-10-23 21:08:10 +0530

    Create AddTwoNumbers.c
    Here, two user inputs are taken for variables 'a' and 'b', using the scanf() function. In scanf(), "%d" is used for reading an integer and & is used to pass the address of the variable.

---
+++ AddTwoNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int a, b;
+  printf("Enter the value for a: ");
+  scanf("%d",&a);
+  printf("Enter the value for b: ");
+  scanf("%d",&b);
+  printf("Sum of %d and %d is: %d\n:",a,b,a+b);
+  return 0;
+}
+  
