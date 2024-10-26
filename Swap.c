8d57b1dbc2c00f714f43b00495a40e324acee7be
Nandinee Sharma <141727975+Nandinee-Sharma@users.noreply.github.com>
2024-10-26 09:47:14 +0530

    Create Swap.c
    Swaps two numbers using an extra variable and methods by passing the arguments by reference.

---
+++ Swap.c
@@ -0,0 +1,21 @@
+#include <stdio.>
+
+int swap(int n1,int n2){
+  int temp;
+  temp =n1;
+  n1 = n2;
+  n2 = temp;
+  printf(n1,n2);
+}
+int main(){
+  int n1=10;
+  int n2=20;
+  printf("Before swapping: ",n1,n2);
+  swap(n1,n2);
+  printf("After swapping: ",n1,n2);
+  return 0;
+}
+
+   
+
+  
