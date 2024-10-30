773909c1030d7a95102649dd96fd682ee66d9ecd
Nandinee Sharma <141727975+Nandinee-Sharma@users.noreply.github.com>
2024-10-30 16:17:31 +0530

    Update Swap.c
    Swaps two numbers, using a swap() function.

--- Swap.c
+++ Swap.c
@@ -1,17 +1,17 @@
-#include <stdio.>
+#include <stdio.h>
 
-int swap(int n1,int n2){
+int swap(int *n1,int *n2){
   int temp;
-  temp =n1;
-  n1 = n2;
-  n2 = temp;
+  temp = *n1;
+  *n1 = *n2;
+  *n2 = temp;
   printf(n1,n2);
 }
 int main(){
   int n1=10;
   int n2=20;
   printf("Before swapping: ",n1,n2);
-  swap(n1,n2);
+  swap(&n1,&n2);
   printf("After swapping: ",n1,n2);
   return 0;
 }
